8ddcc131538178a47fb04e8e308914c590df1253
pz-react-dev <73307007+pz-react-dev@users.noreply.github.com>
2024-01-16 12:26:48 +0100

    Ready to publish

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1705404173/wp-content/uploads/uag-plugin/assets/0/uag-js-408.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-11c02da5' );
+	UAGBTabs.anchorTabId( '.uagb-block-11c02da5' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-11c02da5' );
+}, false );
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-11c02da5' );
+	UAGBTabs.anchorTabId( '.uagb-block-11c02da5' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-11c02da5' );
+}, false );
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-11c02da5' );
+	UAGBTabs.anchorTabId( '.uagb-block-11c02da5' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-11c02da5' );
+}, false );
+ });
